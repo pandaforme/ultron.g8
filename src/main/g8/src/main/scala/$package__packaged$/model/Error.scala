9223b83589fc34bed0dc9031225c67dfa9074326
Chien-Ku, Chen <chienku.chen@agoda.com>
2019-07-25 14:10:27 +0700

    Support tapir

--- src/main/g8/src/main/scala/$package__packaged$/model/Error.scala
+++ src/main/g8/src/main/scala/$package__packaged$/model/Error.scala
@@ -2,5 +2,5 @@ package $package$.model
 
 sealed abstract class Error
 case class UnexpectedError(cause: Throwable) extends Error
-case class ParseJsonError(cause: Exception) extends Error
 case class DBError(cause: Exception) extends Error
+case class NotFoundError(message: String) extends Error
